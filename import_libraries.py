a7e24ee889ad753377bb000bba78b7e8defd0442
Aleksei-Kutuzov <Alekseikutuzov2@yandex.com>
2024-02-03 17:20:02 +0300

    it is libraries file

---
+++ import_libraries.py
@@ -0,0 +1,15 @@
+import pygame as pg
+import copy
+import inspect
+import random
+import threading
+import types
+import sys
+import traceback
+import neat
+import json
+import tkinter.messagebox
+import os
+from tkinter import Tk
+from tkinter.filedialog import askopenfilename
+import sqlite3 as sql
